21b8295b5989e779e73bbf03c32e36762bb87e23
Chong Yidong <cyd@gnu.org>
2016-07-08 11:45:45 +0800

    Tweaks to the widget code.

--- nbsettings.py
+++ nbsettings.py
@@ -5,22 +5,19 @@ import matplotlib.pyplot as plt
 plt.rcParams['savefig.dpi'] = 75
 plt.rcParams['figure.autolayout'] = False
 plt.rcParams['figure.figsize'] = 10, 5
-plt.rcParams['axes.labelsize'] = 20
-plt.rcParams['axes.titlesize'] = 20
+plt.rcParams['axes.labelsize'] = 18
+plt.rcParams['axes.titlesize'] = 16
 plt.rcParams['axes.linewidth'] = 2.0
 plt.rcParams['ytick.major.width'] = 1.4
 plt.rcParams['ytick.minor.width'] = 1.4
-plt.rcParams['ytick.labelsize'] = 16
+plt.rcParams['ytick.labelsize'] = 15
 plt.rcParams['xtick.major.width'] = 1.4
 plt.rcParams['xtick.minor.width'] = 1.4
-plt.rcParams['xtick.labelsize'] = 16
-plt.rcParams['font.size'] = 16
+plt.rcParams['xtick.labelsize'] = 15
+plt.rcParams['font.size'] = 14
 plt.rcParams['lines.linewidth'] = 2.0
 plt.rcParams['lines.markersize'] = 8
 plt.rcParams['legend.fontsize'] = 14
-plt.rcParams['text.usetex'] = True
-plt.rcParams['font.family'] = "serif"
-plt.rcParams['font.serif'] = "cm"
 plt.rcParams['text.latex.preamble'] = "\\usepackage{subdepth}, \\usepackage{type1cm}"
 
 from IPython.display import set_matplotlib_formats
