027d5daf49aa7ee84ea0d9c7c75377fbb90c45b2
Chong Yidong <cyd@gnu.org>
2016-07-14 14:25:54 +0800

    Add files for custom CSS and JS.

---
+++ custom.js
@@ -0,0 +1,18 @@
+code_show=false;
+function code_toggle() {
+    if (code_show) {
+	$('div.input').hide();
+    } else {
+	$('div.input').show();
+    }
+    code_show = !code_show
+}
+
+$([IPython.events]).on('notebook_loaded.Notebook', function() {
+    $("#view_menu").append("<li id=\"toggle_input\" title=\"Show/Hide Code\"><a href=\"javascript:code_toggle()\">Show/Hide Code</a></li>")
+    $('div#ipython_notebook').hide()
+    $('span#save_widget').hide()
+    $('span#kernel_logo_widget').hide()
+    $('div.input').hide()
+    // $('#notebook_panel').append(copyright)
+});
