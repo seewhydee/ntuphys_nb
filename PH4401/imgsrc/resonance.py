296961a4ef213871d8c1465a83760fd548d89a05
Chong Yidong <cyd@gnu.org>
2017-09-20 13:21:26 +0800

    Tweaks to resonance chapter.

--- PH4401/imgsrc/resonance.py
+++ PH4401/imgsrc/resonance.py
@@ -5,15 +5,13 @@ import tmm
 import sys
 
 ## Plot transmittance vs E
-def resonance_demo_1(V0, V1, a, b, Emax):
-    # Evec = linspace(0.01, Emax, 5000)
-
-    Evec = linspace(10, 12, 5000)
+def resonance_demo_1(U, Vb, a, b, Emax):
+    Evec = linspace(0.01, Emax, 5000)
 
     fm   = empty(len(Evec), dtype=complex)
     fp   = empty(len(Evec), dtype=complex)
 
-    L, V = array([b-a, 2*a, b-a]), array([V1, V0, V1])
+    L, V = array([b-a, 2*a, b-a]), array([Vb, Vb-U, Vb])
     for jj in range(len(Evec)):
         k = sqrt(2*Evec[jj])
         M = tmm.transfer_matrix(L, V, 0.0, Evec[jj])
@@ -25,18 +23,20 @@ def resonance_demo_1(V0, V1, a, b, Emax):
     plt.ylim(0,1.05)
     plt.show()
 
-# resonance_demo_1(10.0, 30.0, 1.0, 1.4, 70.0)
+# resonance_demo_1(20.0, 30.0, 1.0, 1.4, 70.0)
 
+
+## Plot resonance wavefunctions
 def resonance_demo_2():
     from scipy.integrate import trapz
-    V0, V1 = 10.0, 30.0
+    U, Vb = 20.0, 30.0
     a, b = 1.0, 1.2
     nstates = 3
     ## From call to Ebound
     Eres = [10.918, 13.646, 18.092, 24.002, 29.973];
 
     ## Plot resonance wavefunctions (left input = 1)
-    L, V = array([b-a, 2*a, b-a]), array([V1, V0, V1])
+    L, V = array([b-a, 2*a, b-a]), array([Vb, Vb-U, Vb])
     for n in range(nstates):
         S = tmm.scattering_matrix(L, V, 0.0, Eres[n])
         components = array([1.0, S[0,0]])
@@ -47,7 +47,7 @@ def resonance_demo_2():
         plt.ylim(0)
 
     ## Plot the corresponding square well bound states
-    L, V = array([2*a]), array([V0-V1])
+    L, V = array([2*a]), array([-U])
     Ebound = tmm.bound_state_energies(L, V)
     inputwave = array([0, 1.0], dtype=complex)
     for n in range(nstates):
@@ -61,17 +61,16 @@ def resonance_demo_2():
         plt.ylim(0,1)
     plt.show()
 
-## Plot transmittance resonance width vs b
+## Plot resonance width vs b, and compare to Fermi's Golden Rule.
 def resonance_demo_3():
-    V0, V1 =10.0, 30.0
-    a = 1.0
-    V = array([V1, V0, V1])
+    U, Vb = 20.0, 30.0
+    a     = 1.0
+    V = array([Vb, Vb-U, Vb])
 
-    ## For given E resolution, b can range from 1.1 to 1.8
+    ## For this E resolution, we can let b range over ~ [1.1, 1.8]
     Evec = linspace(10.6, 11.2, 5000)
-    T    = empty(len(Evec))
-
     bvec = linspace(1.1, 1.6, 20)
+    T    = empty(len(Evec))
     fwhm = empty(len(bvec))
 
     def transmittance(E, dT=0.0):
@@ -85,7 +84,7 @@ def resonance_demo_3():
         L = array([b-a, 2*a, b-a])
         for jj in range(len(Evec)):
             T[jj] = transmittance(Evec[jj])
-        ## Find where T = 0.5
+        ## Locate energies where T = 0.5
         dT  = T - 0.5
         idx = nonzero(dT[:-1] * dT[1:] < 0)[0]
         assert len(idx) == 2
@@ -94,27 +93,26 @@ def resonance_demo_3():
             jj   = idx[n]
             Eres[n] = brentq(transmittance, Evec[jj-1], Evec[jj+2], (0.5,))
         fwhm[ii] = Eres[1] - Eres[0]
-
         sys.stdout.write('\r' + str(ii+1) + '/' + str(len(bvec)))
         sys.stdout.flush()
     sys.stdout.write('\n')
-    plt.plot(bvec, fwhm, 'o')
+    plt.plot(bvec-a, fwhm, 'o')
 
     ## Fermi's Golden Rule prediction
-    L, V = array([2*a]), array([V0-V1])
+    L, V = array([2*a]), array([-U])
     E0  = tmm.bound_state_energies(L, V)[0]
-    dd  = pi/2 - sqrt(2*(E0+V1-V0))
+    q   = sqrt(2*(E0+U))
     et  = sqrt(-2*E0)
-    k   = sqrt(2*(E0+V1))
-    DOS = sqrt(2/(E0+V1))
-    Bsq = exp(2*et*a) * dd*dd/a * pi / (1+pi)
+    k   = sqrt(2*(E0+Vb))
+    DOS = sqrt(2/(E0+Vb))
+    Bsq = exp(2*et*a)/a/((1+sin(2*q*a)/2/q/a)/cos(q*a)**2 + 1/et/a)
 
     bb = linspace(1.05, 1.8, 200)
     overlap = (et*cos(k*bb) - k*sin(k*bb))**2 * exp(-2*et*bb) / (2*pi)
     K = 2*pi * Bsq * overlap * DOS
 
-    plt.plot(bb, K)
-    plt.xlim(1, 1.6)
+    plt.plot(bb-a, K)
+    plt.xlim(0, 0.6)
     plt.ylim(0, 0.35)
     plt.show()
 
